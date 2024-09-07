48be1e0f5e084018f5124bdf4e90eab1632cb840
AgentKisa <155394878+AgentKisa@users.noreply.github.com>
2024-09-07 14:34:20 +0300

    contacts

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,15 @@
-export const removeLastContact = async () => {};
+import fs from 'node:fs/promises';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+    const newContacts = contacts.slice(0, contacts.length - 1);
+    await writeContacts(newContacts);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 removeLastContact();
